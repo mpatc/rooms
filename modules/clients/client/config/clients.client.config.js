102abd560ff2f247d1f460def7d5bb07969aa2b5
mpatc <matthewpclancy@gmail.com>
2016-05-03 21:09:52 -0700

    first

---
+++ modules/clients/client/config/clients.client.config.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('clients')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['Menus'];
+
+  function menuConfig(Menus) {
+    // Set top bar menu items
+    Menus.addMenuItem('topbar', {
+      title: 'Clients',
+      state: 'clients',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    Menus.addSubMenuItem('topbar', 'clients', {
+      title: 'List Clients',
+      state: 'clients.list'
+    });
+
+    // Add the dropdown create item
+    Menus.addSubMenuItem('topbar', 'clients', {
+      title: 'Create Client',
+      state: 'clients.create',
+      roles: ['user']
+    });
+  }
+})();
